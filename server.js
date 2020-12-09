8c7d0e9d06cd1ed7c3db3444de2e7d9fb070a412
= <=>
2020-12-09 14:04:07 +0530

    add in api

--- server.js
+++ server.js
@@ -108,8 +108,8 @@ app.get("/Allproducts", (req, res) => {
 app.get("/products/category/:key", (req, res) => {
 
     app.use((req,res,next)=>{
-        res.header('Access-Control-Allow-Origin','*')
-        res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept")
+        res.setHeader('Access-Control-Allow-Origin','*')
+        res.setHeader('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept")
         next();
     })
   
