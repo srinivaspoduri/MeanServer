8b7f37a3c30ad72f1dc25cc13f540a7e1c4be693
= <=>
2020-12-09 13:40:04 +0530

    updated

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ let cors = require("cors")
 app.use(cors())
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept")
     next();
 })
 //to create client to connect db from server(node)
